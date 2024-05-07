9a09c5017cf335bee6e4915bbcd2de278803b59e
ikcadrus <jakub0901.js@gmail.com>
2024-05-08 01:02:02 +0200

    Create map component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { ConverterComponent } from './components/converter/converter.component';
+import { MapComponent } from './components/map/map.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, NavbarComponent, ConverterComponent]
+    imports: [RouterOutlet, NavbarComponent, ConverterComponent, MapComponent]
 })
 export class AppComponent {
   title = 'coinvert';
